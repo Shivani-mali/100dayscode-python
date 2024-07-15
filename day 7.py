7c1891238ab47298003fe3a7f714cf407c343fd3
Shivani Mali <shivanimali339@gmail.com>
2024-07-15 16:57:44 +0530

    Add files via upload

---
+++ day 7.py
@@ -0,0 +1,26 @@
+# EXERCISE 1:-CALCULATOR USING PYTHON:-
+# Operators
+# Python has different types of operators for different operations. To create a calculator we require arithmetic operators.
+
+# Arithmetic operators
+# Operator	Operator Name	Example
+# +	Addition	15+7
+# -	Subtraction	15-7
+# *	Multiplication	5*7
+# **	Exponential	5**3
+# /	Division	5/3
+# %	Modulus	15%7
+# //	Floor Division	15//7
+
+print(5+6)
+print(15-6)
+print(15*9)
+print(16/7)
+print(456//67)
+print(15%6)  #it's give the remainder.
+print(3**5)
+
+#Exercise 1:- Create a calculator capable of performing addition,substraction,multiplication and division 
+# operation on two numbers . Your program should format the output in a readable manner.
+
+# solution in day8 file..
---
+++ day 8.py
@@ -0,0 +1,22 @@
+#Exercise 1:- Create a calculator capable of performing addition,substraction,multiplication and division 
+# operation on two numbers . Your program should format the output in a readable manner.
+
+# Make the simple calculator:-
+
+a =45
+b =3
+print("The value of",a," + ", b, "is:", a+b)
+print("The value of",a," % ", b, "is:", a%b)
+print("The value of",a," - ", b, "is:", a-b)
+print("The value of",a," * ", b, "is:", a*b)
+print("The value of",a," / ", b, "is:", a/b)
+
+a =465465367
+b =5654
+print("The value of",a," + ", b, "is:", a+b)
+print("The value of",a," % ", b, "is:", a%b)
+print("The value of",a," - ", b, "is:", a-b)
+print("The value of",a," * ", b, "is:", a*b)
+print("The value of",a," / ", b, "is:", a/b)
+
+# like that you can make the THE SIMPLE CALCULATOR...
