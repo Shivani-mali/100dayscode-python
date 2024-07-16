c0d01bf8d92ca28ecd59e28b016e58f8ef99b1c1
Shivani Mali <shivanimali339@gmail.com>
2024-07-16 23:10:54 +0530

    100daysofcode-python-what I learn

---
+++ day 9.py
@@ -0,0 +1,31 @@
+# TYPE CASTING:-
+a =3
+b =6
+print(a+b)
+#upper is an integer so it only print !
+print("a+b")
+#upper all stuff is  string and it's known as a typecasting. 
+# Typecasting in python:-
+# The conversion of one data type into the other data type is known as type casting in python or type conversion in python.
+
+# Python supports a wide variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc. for the type casting in python.
+
+# Two Types of Typecasting:
+# Explicit Conversion (Explicit type casting in python)
+# Implicit Conversion (Implicit type casting in python).
+
+
+# Explicit Conversion (Explicit type casting in python):-
+# for eg:-conversion of "STRING" into "INTEGER". by the programmer.
+string = "15"
+number = 7
+string_number = int(string) #throws the error if the string is not valid integer.
+sum = number + string_number
+print("The sum of the both of the numbers is:",sum) 
+
+# Implicit Conversion (Implicit type casting in python):-
+# for e.g:-by the python...
+c =32131.54
+d =54
+print(c +d)
+#thta's all about type casting.
