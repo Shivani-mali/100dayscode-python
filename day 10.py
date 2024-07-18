7bf79d0d74dc512d62128f24d4bac3c849e18d43
Shivani Mali <shivanimali339@gmail.com>
2024-07-18 22:37:02 +0530

    100dayscode-python ,that's all about what I learn.
    this is a awesome journey for me  so, I can, I will ,bounce back!!!

---
+++ day 10.py
@@ -0,0 +1,27 @@
+# # Taking User Input in Python:-
+# # syntax of python:-
+# variable = input()
+# #For ex:-
+# a = input()
+# print("My name is",a)
+# #Also,You can write as:-
+# a = input("Emter your name")
+# print("My name is",a)
+
+x = input("Enter the first number:") #it consider as input string.
+y = input("Enter the second number:")
+print(x + y)
+# |it is also writeen as:-
+print(int(x) + int(y))    #this is a right way to write calculation..
+
+#Arithmatic calculation in the form of "user input"
+x = input("Enter the first number:") 
+y = input("Enter the second number:")
+
+print(int(x) + int(y))    
+print(int(x) - int(y)) 
+print(int(x) * int(y)) 
+print(int(x) / int(y)) 
+print(int(x) % int(y))
+
+#That is all about the user input value...
