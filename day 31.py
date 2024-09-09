4fe482eb4b9711bfc6f819f81376c4d5a0fb2514
Shivani Mali <shivanimali339@gmail.com>
2024-09-09 23:03:39 +0530

    Set in python

---
+++ day 31.py
@@ -0,0 +1,17 @@
+# Sets:
+# It is a collections of well defined-objects
+s = {2, 4, 2, 6}
+print(s)
+# The values are not repeated.u seen in this example.
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+# HEre order is not maintain!
+
+# Try to create a empaty set
+shivani = set()
+print(type(shivani))  #it is make empaty set
+# or either it's make the value of class.
+
+for value in info:
+    print(value)
